7be19e4c97fc514d0d5805a4a1e8f6741308ef5d
Jennifer Lindner <jen@jenlindner.com>
2010-10-11 22:05:57 -0500

    comic index and show actions and views

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -6,6 +6,12 @@ class ComicsController < ApplicationController
   
   def create
     @comic = Comic.new
+    @comic.title = params[:comic][:title]
+    if @comic.save
+      redirect_to @comic
+    else
+      redirect_to :action => :index
+    end
   end
   
   def show
