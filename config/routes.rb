2887cad3da9170af727d6b73f9b22fac4ac82158
Jen Lindner <jen@jenlindner.com>
2010-11-09 12:48:21 -0600

    mapped root, deleted static index, corrected typefont embed code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :comics, :member => {:reorder => :post} do |comic| 
+
+ map.root :controller => :comics 
+ 
+ map.resources :comics, :member => {:reorder => :post} do |comic| 
     comic.resources :panels, :member => { :comicify => :post,  
                     :image => :get, :modified_image => :get, :text => :put}
   end
-  
-  map.root :controller => :comics, :action => :index
+ 
 end
 
